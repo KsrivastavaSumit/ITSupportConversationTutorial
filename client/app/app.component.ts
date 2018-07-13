17c3d71b48931624e391eb71147657bde5548726
Ksrivastava.Sumit <skumar77@CSCINDAH506175.asiapac.globalcsc.net>
2018-07-13 14:17:52 +0530

    tyling changes mainly

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  version:string ="v0.0.1 16/25/2018"
+  version:string ="v0.0.1 07/10/2018"
 }
